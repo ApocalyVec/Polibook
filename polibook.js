b173154832261e4efb504d27af559053d91450c6
ApocalyVec <s-vector.lee@hotmail.com>
2019-05-28 23:57:56 -0400

    implement drawing continuous line with altering color

--- polibook.js
+++ polibook.js
@@ -6,11 +6,85 @@ var program;
 var points = [];
 var colors = [];
 var curColor = 'd';  // can be 'b', 'r', 'g', 'b'
-var lines;
+var lines = null;
+
+var linesDMdoe = [];
+var pointsDMode = [];
+var colorsDMode = [];
+
+var mode = 'f';  // default mode is file mode
 
 Array.prototype.insert = function ( index, item ) { // source https://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index-javascript
     this.splice( index, 0, item );
 };
+function render() {  // render points and color
+
+    // create Points GPU buffer
+    var pBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);  // flatten the points to be 1D data
+
+    var vPosition = gl.getAttribLocation(program,  "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // create Color GPU buffer
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program,  "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    if(points.length == 1) {
+        gl.drawArrays(gl.POINTS, 0, points.length);
+    }
+    else {
+        gl.drawArrays(gl.LINE_STRIP, 0, points.length);
+    }
+}
+
+function renderDMode() {
+    // Set clear color
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // Clear <canvas> by clearning the color buffer
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    for(let i = 0; i < linesDMdoe.length; i ++) {
+        // create Points GPU buffer
+
+        colorsDMode = []
+        for (let j = 0; j < linesDMdoe[i].length; j ++) {
+            pushToColor(colorsDMode);
+        }
+
+        var pBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(linesDMdoe[i]), gl.STATIC_DRAW);  // flatten the points to be 1D data
+
+        var vPosition = gl.getAttribLocation(program,  "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+
+        // create Color GPU buffer
+        var cBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsDMode), gl.STATIC_DRAW);
+
+        var vColor = gl.getAttribLocation(program,  "vColor");
+        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vColor);
+
+        if(linesDMdoe[i].length == 1) {
+            gl.drawArrays(gl.POINTS, 0, linesDMdoe[i].length);
+        }
+        else {
+            gl.drawArrays(gl.LINE_STRIP, 0, linesDMdoe[i].length);
+        }
+    }
+}
 
 function polibook_draw(lines) {
 
@@ -85,25 +159,7 @@ function polibook_draw(lines) {
                 if(points.length != 0) { // draw the line if there is any
                     console.log("drawing line: " + points);
 
-                    // create Points GPU buffer
-                    var pBuffer = gl.createBuffer();
-                    gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-                    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);  // flatten the points to be 1D data
-
-                    var vPosition = gl.getAttribLocation(program,  "vPosition");
-                    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-                    gl.enableVertexAttribArray(vPosition);
-
-                    // create Color GPU buffer
-                    var cBuffer = gl.createBuffer();
-                    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-                    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-                    var vColor = gl.getAttribLocation(program,  "vColor");
-                    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-                    gl.enableVertexAttribArray(vColor);
-
-                    gl.drawArrays(gl.LINE_STRIP, 0, points.length);
+                    render();
                 }
                 points = [];
                 colors = [];
@@ -116,23 +172,8 @@ function polibook_draw(lines) {
                 y = parseFloat(lines[j][1])
 
                 points.push(vec4(x, y, 0.0, 1.0));
+                pushToColor(colors);
 
-                switch (curColor) {
-                    case 'd':
-                        colors.push(vec4(0.0, 0.0, 0.0, 1.0));
-                        break;
-                    case 'r':
-                        colors.push(vec4(1.0, 0.0, 0.0, 1.0));
-                        break;
-                    case 'g':
-                        colors.push(vec4(0.0, 1.0, 0.0, 1.0));
-                        break;
-                    case 'b':
-                        colors.push(vec4(0.0, 0.0, 1.0, 1.0));
-                        break;
-                    default:
-                         colors.push(vec4(0.0, 0.0, 0.0, 1.0));
-                }
 
                 console.log('Adding point: [' + x + ',' + y + ']')
                 }
@@ -141,31 +182,31 @@ function polibook_draw(lines) {
 
     if(points.length != 0) { // draw the line if there is any
         console.log("drawing line: " + points);
-
-        // create Points GPU buffer
-        var pBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);  // flatten the points to be 1D data
-
-        var vPosition = gl.getAttribLocation(program,  "vPosition");
-        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(vPosition);
-
-        // create Color GPU buffer
-        var cBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-        var vColor = gl.getAttribLocation(program,  "vColor");
-        gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(vColor);
-
-        gl.drawArrays(gl.LINE_STRIP, 0, points.length);
+        render()
     }
     points = [];
     colors = [];
 
     return 0;
+}
+
+function pushToColor(colorList) { // push to colors depending on cur color
+    switch (curColor) {
+    case 'd':
+        colorList.push(vec4(0.0, 0.0, 0.0, 1.0));
+        break;
+    case 'r':
+        colorList.push(vec4(1.0, 0.0, 0.0, 1.0));
+        break;
+    case 'g':
+        colorList.push(vec4(0.0, 1.0, 0.0, 1.0));
+        break;
+    case 'b':
+        colorList.push(vec4(0.0, 0.0, 1.0, 1.0));
+        break;
+    default:
+         colorList.push(vec4(0.0, 0.0, 0.0, 1.0));
+}
 }
 
 function main()
@@ -199,8 +240,10 @@ function main()
 
     // Retrieve <canvas> element
 	var canvas = document.getElementById('webgl');
-	// Retrieve <mode text field
+	// Retrieve <mode text> field
     var modeTf = document.getElementById('modeTf')
+    // Retrieve <upload file box> field
+    var uploadBox = document.getElementById('uploadBox');
 
 	// Get the rendering context for WebGL, comes from the libraries, everything we do will be in gl
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -273,13 +316,56 @@ function main()
 	// // Clear <canvas> by clearning the color buffer
 	// gl.clear(gl.COLOR_BUFFER_BIT);
 
+    canvas.addEventListener("mousedown", function(event) {
+        // let x = event.offsetX;
+        // let y = event.offsetY;
+
+        console.log("Click at: [" + event.offsetX/canvas.width + "," + event.offsetY/canvas.height + "]")
+
+        // draw the point if we are in the draw mode
+        if(mode == 'd') {
+            linesDMdoe.pop();
+            pointsDMode.push(vec4(event.offsetX/canvas.width, (canvas.height - event.offsetY)/canvas.height, 0.0, 1.0));
+            linesDMdoe.push(pointsDMode);
+            // color is dealt in render
+
+            renderDMode();
+
+        }
+    }, false);
+
     window.addEventListener("keypress", function(e) {
         console.log('keypress: ' + e.key);
         if(e.key == 'f') {
-            console.log("File Mode Enabled")
+
+            console.log("File Mode Enabled");
+            modeTf.innerHTML  = "File Mode";
+            mode = 'f';
+            uploadBox.style.display = "block";  // show the upload file box in file mode
+
         }
         else if (e.key == 'd') {
             console.log("Drawing Mode Enabled")
+            modeTf.innerHTML  = "Drawing Mode";
+            mode = 'd';
+            uploadBox.style.display = "none";  // hide the upload file box in drawing mode
+
+            //clear the screen when entering draw mode
+            // Set clear color
+            gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+            // Clear <canvas> by clearning the color buffer
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            // set projection matrix for draw mode
+            var projMatrix = ortho(0.0, 1.0, 0.0, 1.0, -1, 1);
+            var projMatrixLoc = gl.getUniformLocation(program, "projMatrix");
+            gl.uniformMatrix4fv(projMatrixLoc, false, flatten(projMatrix));
+
+            // reset points and color
+            points = [];
+            colors = [];
+            pointsDMode=[]
         }
         else if (e.key == 'c') {
             if(curColor == 'd') {
@@ -294,7 +380,18 @@ function main()
             else if(curColor == 'b') {
                 curColor = 'd';
             }
-            polibook_draw(lines)
+
+            if(mode == 'f') {
+                if(lines) {
+                    polibook_draw(lines)
+                }
+            }
+            else if (mode == 'd') {
+                if(linesDMdoe) {
+                    renderDMode();
+                }
+            }
+
         }
     }, false);
 
